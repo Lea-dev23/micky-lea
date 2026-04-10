e04cdccad3543ade04f089e6e0e08c385b0596e3
lea-dev23 <Magakamtchindaleaanaelle@gmail.com>
2026-04-10 18:19:18 +0100

    changer de message

--- src/App.jsx
+++ src/App.jsx
@@ -105,7 +105,7 @@ function App() {
                 >
                   <use href="/icons.svg#bluesky-icon"></use>
                 </svg>
-                Bluesky
+              mon portfolio
               </a>
             </li>
           </ul>
